0f505dbb52f8684d7c4ac154a1c24c521d5dec1f
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-19 17:13:21 +0300

    rgrgr

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from tkinter import *
-from player_vs_player import player_vs_player_fun_start
+from player_vs_player import player_vs_player_fun_game
 root = Tk()
 root.geometry("600x500")
 def remove_buttons_player_vs_player():
-    player_vs_player_fun_start()
     player_vs_player_button.place(x=-100,y=-100)
     player_vs_PC_button.place(x=-100,y=-100)
+    render_button_games()
+    text_lab.place(x=200 , y =50)
+    root.config(bg="#eeff61")
 def remove_buttons_player_vs_PC():
     # player_vs_player_fun_start()
     player_vs_player_button.place(x=-100,y=-100)
     player_vs_PC_button.place(x=-100,y=-100)
+    render_button_games()
+    text_lab.place(x=200 , y =50)
+    root.config(bg="#eeff61")
 
 player_vs_player_button = Button(text="player_vs_player", command=remove_buttons_player_vs_player)
 player_vs_player_button.place(x=0,y=0)
@@ -21,23 +26,36 @@ player_vs_PC_button.place(x=0,y=30)
 
 
 text_lab = Label(text="None",font=("Arial", 14))
-text_lab.place(x=200 , y =50)
 # https://haxor.no/en/article/tkinter-events
+move_symbol = True
 def fun_games(event):
+    global move_symbol
     button_id = event.widget
-    # text_lab.config(text=button_id)
     if(button_id["text"] == " "):
-        button_id.config(text= "x")
+        move_symbol
+        if(move_symbol):
+            move_symbol = False
+            button_id.config(text= "x")
+            root.config(bg="#37b9fb")
+        else:
+            move_symbol = True
+            button_id.config(text= "o")
+            root.config(bg="#eeff61")
+            
+        list_answer = player_vs_player_fun_game(dist_button_games)
+        text_lab.config(text=list_answer[0])
+        if(list_answer[1] == "ok"):
+            dist_button_games.clear()
     
 
 
 dist_button_games = {}
-for x_index in range (1,4):
-    print(x_index)
-    for y_index in range (1,4):
-        dist_button_games[f'button_X{x_index}_Y{y_index}'] = Button(text=" ", bg="#fcb678" )
-        dist_button_games[f'button_X{x_index}_Y{y_index}'].place(x=140 + (60 * x_index),y=100 + (60 * y_index) , width=50, height=50)
-        dist_button_games[f'button_X{x_index}_Y{y_index}'].bind("<Button-1>" , fun_games)
+def render_button_games():
+    for x_index in range (1,4):
+        for y_index in range (1,4):
+            dist_button_games[f'button_X{x_index}_Y{y_index}'] = Button(text=" ", bg="#fcb678" , font=("Arial", 25))
+            dist_button_games[f'button_X{x_index}_Y{y_index}'].place(x=140 + (60 * x_index),y=100 + (60 * y_index) , width=50, height=50)
+            dist_button_games[f'button_X{x_index}_Y{y_index}'].bind("<Button-1>" , fun_games)
 
 
 print(dist_button_games)
--- player_vs_player.py
+++ player_vs_player.py
@@ -1,2 +1,52 @@
-def player_vs_player_fun_start():
-    print("ejejje")
+def player_vs_player_fun_game(dist_button):
+    str_o_x = "x"
+    for i in range(2):
+        if(i == 1):
+            str_o_x = "o"
+        if (dist_button['button_X1_Y1']["text"] == str_o_x and dist_button['button_X1_Y2']["text"] == str_o_x and dist_button['button_X1_Y3']["text"] == str_o_x):
+            dist_button['button_X1_Y1'].config(bg="#FF4040")
+            dist_button['button_X1_Y2'].config(bg="#FF4040")
+            dist_button['button_X1_Y3'].config(bg="#FF4040")
+            return [str_o_x , "ok"]
+
+        if (dist_button['button_X2_Y1']["text"] == str_o_x and dist_button['button_X2_Y2']["text"] == str_o_x and dist_button['button_X2_Y3']["text"] == str_o_x):
+            dist_button['button_X2_Y1'].config(bg="#FF4040")
+            dist_button['button_X2_Y2'].config(bg="#FF4040")
+            dist_button['button_X2_Y3'].config(bg="#FF4040")
+            return [str_o_x , "ok"]
+
+        if (dist_button['button_X3_Y1']["text"] == str_o_x and dist_button['button_X3_Y2']["text"] == str_o_x and dist_button['button_X3_Y3']["text"] == str_o_x):
+            dist_button['button_X3_Y1'].config(bg="#FF4040")
+            dist_button['button_X3_Y2'].config(bg="#FF4040")
+            dist_button['button_X3_Y3'].config(bg="#FF4040")
+            return [str_o_x , "ok"]
+
+        if (dist_button['button_X1_Y1']["text"] == str_o_x and dist_button['button_X2_Y1']["text"] == str_o_x and dist_button['button_X3_Y1']["text"] == str_o_x):
+            dist_button['button_X1_Y1'].config(bg="#FF4040")
+            dist_button['button_X2_Y1'].config(bg="#FF4040")
+            dist_button['button_X3_Y1'].config(bg="#FF4040")
+            return [str_o_x , "ok"]
+
+        if (dist_button['button_X1_Y2']["text"] == str_o_x and dist_button['button_X2_Y2']["text"] == str_o_x and dist_button['button_X3_Y2']["text"] == str_o_x):
+            dist_button['button_X1_Y2'].config(bg="#FF4040")
+            dist_button['button_X2_Y2'].config(bg="#FF4040")
+            dist_button['button_X3_Y2'].config(bg="#FF4040")
+            return [str_o_x , "ok"]
+
+        if (dist_button['button_X1_Y3']["text"] == str_o_x and dist_button['button_X2_Y3']["text"] == str_o_x and dist_button['button_X3_Y3']["text"] == str_o_x):
+            dist_button['button_X1_Y3'].config(bg="#FF4040")
+            dist_button['button_X2_Y3'].config(bg="#FF4040")
+            dist_button['button_X3_Y3'].config(bg="#FF4040")
+            return [str_o_x , "ok"]
+
+        if (dist_button['button_X1_Y1']["text"] == str_o_x and dist_button['button_X2_Y2']["text"] == str_o_x and dist_button['button_X3_Y3']["text"] == str_o_x):
+            dist_button['button_X1_Y1'].config(bg="#FF4040")
+            dist_button['button_X2_Y2'].config(bg="#FF4040")
+            dist_button['button_X3_Y3'].config(bg="#FF4040")
+            return [str_o_x , "ok"]
+
+        if (dist_button['button_X3_Y1']["text"] == str_o_x and dist_button['button_X2_Y2']["text"] == str_o_x and dist_button['button_X1_Y3']["text"] == str_o_x):
+            dist_button['button_X3_Y1'].config(bg="#FF4040")
+            dist_button['button_X2_Y2'].config(bg="#FF4040")
+            dist_button['button_X1_Y3'].config(bg="#FF4040")
+            return [str_o_x , "ok"]
